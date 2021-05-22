60bc397b8f618ea6258a311add074290fa6ca07c
Giovanni Merlino <gmerlino@unime.it>
2021-05-22 17:10:04 +0200

    style fixes

--- valgrind.rb
+++ valgrind.rb
@@ -1,9 +1,12 @@
+# typed: false
+# frozen_string_literal: true
+
 class Valgrind < Formula
   desc "Dynamic analysis tools (memory, debug, profiling)"
   homepage "http://www.valgrind.org/"
 
   head do
-    url "https://github.com/Echelon9/valgrind.git", :branch => "feature/v3.14/macos-mojave-support-v2"
+    url "https://github.com/Echelon9/valgrind.git", branch: "feature/v3.14/macos-mojave-support-v2"
 
     depends_on "autoconf" => :build
     depends_on "automake" => :build
@@ -19,15 +22,15 @@ class Valgrind < Formula
       --disable-dependency-tracking
       --prefix=#{prefix}
     ]
-#    args << "--enable-only64bit" << "--build=amd64-darwin"
+    #    args << "--enable-only64bit" << "--build=amd64-darwin"
     args << "--enable-only64bit"
 
     system "./autogen.sh" if build.head?
 
     # Look for headers in the SDK on Xcode-only systems: https://bugs.kde.org/show_bug.cgi?id=295084
-#    unless MacOS::CLT.installed?
-      inreplace "coregrind/Makefile.in", %r{(\s)(?=/usr/include/mach/)}, '\1'+MacOS.sdk_path.to_s
-#    end
+    #    unless MacOS::CLT.installed?
+    inreplace "coregrind/Makefile.in", %r{(\s)(?=/usr/include/mach/)}, "\\1#{MacOS.sdk_path}"
+    #    end
 
     system "./configure", *args
     system "make"
